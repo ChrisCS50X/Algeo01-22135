ae51c96a98f8b5c78f766d44ccaae60679df0b45
Christian Justin <christian.justin23@gmail.com>
2023-09-21 15:43:58 +0700

    Added CreateIdentity and Multiply

--- src/functions/Matrix.java
+++ src/functions/Matrix.java
@@ -1,7 +1,16 @@
 package functions;
 
 public class Matrix {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static double[][] CreateIdentityMatrix(double[][] M, int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == j) {
+                    M[i][j] = 1;
+                } else {
+                    M[i][j] = 0;
+                }
+            }
+        }
+        return M;
     }
 }
--- src/functions/operations.java
+++ src/functions/operations.java
@@ -18,5 +18,21 @@ public class operations {
             }
         }
         return extended;
-    }   
+    }
+    
+    public static double [][] MultiplyMatrix (double[][] matrix1, double[][] matrix2){
+        int row = matrix1.length;
+        int col = matrix2 [0].length;
+        int i,j,k;
+        double [][] m3 = new double [row] [col];
+        for (i = 0 ; i < row ; i++){
+            for (j = 0 ; j < col ; j++){
+                m3[i][j] = 0;
+                for (k = 0 ; k < matrix1[0].length ; k++){
+                    m3[i][j] += matrix1[i][k] * matrix2[k][j];
+                }
+            }
+        }
+        return m3;
+    } 
 }

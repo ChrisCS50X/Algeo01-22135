38670c78bd11443ed589685dc925c2aabde98d7b
JasonFernandoo <j450n.fernando@gmail.com>
2023-09-21 13:44:26 +0700

    fungsi extend matrix

--- src/functions/operations.java
+++ src/functions/operations.java
@@ -1,2 +1,22 @@
 package functions;
 
+public class operations {
+    public static int[][] extendMatrix(int[][] Matrix1, int[][] Matrix2) {
+        int[][] extended;
+        int Row, Column, i, j;
+        Row = Matrix1.length;
+        Column = Matrix1[0].length + Matrix2[0].length;
+
+        extended = new int[Row][Column];
+        for (i = 0; i < Row; i++) {
+            for (j = 0; j < Column; j++){
+                if (j < Matrix1[0].length){
+                    extended[i][j] = Matrix1[i][j];
+                } else {
+                    extended[i][j] = Matrix2[i][j-Matrix1[0].length];
+                }
+            }
+        }
+        return extended;
+    }   
+}

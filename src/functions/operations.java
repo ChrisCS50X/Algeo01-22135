3e79f3a231ff2f11d5dd3f648d5d8c7c014d2c38
Christian Justin <christian.justin23@gmail.com>
2023-09-23 10:48:45 +0700

    Update operations.java

--- src/functions/operations.java
+++ src/functions/operations.java
@@ -1,13 +1,13 @@
 package functions;
 
 public class operations {
-    public static int[][] extendMatrix(int[][] Matrix1, int[][] Matrix2) {
-        int[][] extended;
+    public static double[][] extendMatrix(double[][] Matrix1, double[][] Matrix2) {
+        double[][] extended;
         int Row, Column, i, j;
         Row = Matrix1.length;
         Column = Matrix1[0].length + Matrix2[0].length;
 
-        extended = new int[Row][Column];
+        extended = new double[Row][Column];
         for (i = 0; i < Row; i++) {
             for (j = 0; j < Column; j++){
                 if (j < Matrix1[0].length){

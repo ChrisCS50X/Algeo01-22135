7f34afa0232d85a3a0b6227bc1560c78cc5d5e95
Christian Justin <christian.justin23@gmail.com>
2023-09-24 21:06:25 +0700

    Update Matrix.Java
    Added IsSquare, OutputMatrix. and CopyMatrix

--- src/functions/Matrix.java
+++ src/functions/Matrix.java
@@ -1,6 +1,18 @@
 package functions;
+import java.util.Scanner;
 
 public class Matrix {
+    public static Scanner scan;
+
+    public static void OutputMatrix(double[][] M){
+        for (int i = 0; i < M.length;i++){
+            for (int j = 0; j< M[i].length;j++){
+                System.out.print(M[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
     public static double[][] CreateIdentityMatrix(double[][] M, int n) {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
@@ -13,4 +25,32 @@ public class Matrix {
         }
         return M;
     }
+
+    public static boolean IsSquare(double[][] matrix) {
+        int nRows = matrix.length;
+        int nCols = matrix[0].length;
+    
+        for (int i = 1; i < nRows; i++) {
+            if (matrix[i].length != nCols) {
+                return false;
+            }
+        }
+    
+        return true;
+    }
+
+    public static double[][] copyMatrix(double[][] matrix) {
+        int nRows = matrix.length;
+        int nCols = matrix[0].length;
+        
+        double[][] copy = new double[nRows][nCols];
+        for (int i = 0; i < nRows;i++ ){
+            for (int j = 0; j < nCols; j++){
+                copy [i][j] = matrix[i][j];
+            }
+        }
+        return copy;
+    }
+
+    
 }

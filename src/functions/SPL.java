49b5dbd9f62bd560ad43869e98771125479843d0
JasonFernandoo <j450n.fernando@gmail.com>
2023-10-05 17:19:50 +0700

    pembetulan SPL

--- src/functions/SPL.java
+++ src/functions/SPL.java
@@ -1,85 +1,15 @@
 package functions;
 
-import userinterference.outputMatrix;
-
 public class SPL {
     
     public static double[][] gaussElim(double[][] matrix) {
-        int Row = matrix.length;
-        int Column = matrix[0].length;
-
-        for (int pivot = 0; pivot < Row; pivot++ ) {
-            // pivot value fungsinya untuk mengetahui nilai-nilai pada diagonal utama
-            double pivotVal = matrix[pivot][pivot];
-
-            for (int col = 0; col < Column; col++) {
-                // fungsi ini merupakan sebuah operasi dimana satu baris akan dibagi nilainya dengan nilai yang sudah ada pada pivot Value
-                if(matrix[pivot][col] != 0) {
-                    if(pivotVal == 0) {
-                        matrix[pivot][col] /= 1;
-                    }
-                    else {
-                       matrix[pivot][col] /= pivotVal; 
-                    }
-                }
-                else {
-                    matrix[pivot][col] = 0;
-                }
-            }
-            
-            for (int row = 0; row < Row; row++) {
-                // fungsi ini beroperasi pada baris dibawah pivot jadi baris diatas yang sudah ada satu utama tidak diganti
-                if (row > pivot) {
-                    double temp = matrix[row][pivot];
-
-                    for (int col = 0; col < Column; col++) {
-                        //nilai matrix yang dibawah pivot akan dikurangi dengan dirinya sendiri yang membuat nilainya 0
-                        matrix[row][col] -= temp * matrix[pivot][col];
-                    }
-                    outputMatrix.OutString(matrix);
-                }
-            }
-        }
+        matrix = operations.OBE(matrix);
         return matrix;
     }
 
 
     public static double[][] gaussJordanElim(double[][] matrix) {
-        int Row = matrix.length;
-        int Column = matrix[0].length;
-
-        for (int pivot = 0; pivot < Row; pivot++ ) {
-            // pivot value fungsinya untuk mengetahui nilai-nilai pada diagonal utama
-            double pivotVal = matrix[pivot][pivot];
-
-            for (int col = 0; col < Column; col++) {
-                // fungsi ini merupakan sebuah operasi dimana satu baris akan dibagi nilainya dengan nilai yang sudah ada pada pivot Value
-                if(matrix[pivot][col] != 0) {
-                    if(pivotVal == 0) {
-                        matrix[pivot][col] /= 1;
-                    }
-                    else {
-                       matrix[pivot][col] /= pivotVal; 
-                    }
-                }
-                else {
-                    matrix[pivot][col] = 0;
-                }
-            }
-
-            for (int row = 0; row < Row; row++) {
-                // fungsi ini beroperasi pada baris selain baris pivot yang sedang dikerjakan
-                if (row != pivot) {
-                    double temp = matrix[row][pivot];
-
-                    for (int col = 0; col < Column; col++) {
-                        //nilai matrix yang dibawah pivot akan dikurangi dengan dirinya sendiri yang membuat nilainya 0
-                        matrix[row][col] -= temp * matrix[pivot][col];
-                    }
-
-                }
-            }
-        }
+        matrix = operations.OBE_Tereduksi(matrix);
         return matrix;
     }
 
